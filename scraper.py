21b0139f5183cd17839cadecccd65ca6a56d4051
Jon Nguyen <jon.mail@hotmail.com>
2020-06-20 22:34:36 -0700

    basic scraper and notebook

---
+++ scraper.py
@@ -0,0 +1,23 @@
+import requests
+import os
+import time
+import numpy as np
+
+# Configure dates to search. Only counts business days non-holiday/break
+# API call url taken from XHR response
+start_date = '2020-07-06'
+end_date = '2020-08-20'
+date_url = f'https://calendly.com/api/booking/event_types/EEAPAX3DZQ2LU2G7/calendar/range?timezone=America%2FLos_Angeles&diagnostics=false&range_start={start_date}&range_end={end_date}&single_use_link_uuid='
+
+
+r = requests.get(date_url)
+response = r.json()
+
+# Business days being returned (doesn't account for break)
+# Excludes last date
+days = np.busday_count(start_date, end_date)
+
+for i in range(0, days + 1):
+  if response['days'][i]['status'] == 'available':
+    date = response['days'][i]['date']
+    print(f"{date} has available slots")
