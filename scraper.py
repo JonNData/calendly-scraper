b4297edaa16573790b9b0658adad7cbc8cfa3861
Jon Nguyen <jon.mail@hotmail.com>
2020-06-21 01:17:11 -0700

    added voice

--- scraper.py
+++ scraper.py
@@ -2,11 +2,12 @@ import requests
 import os
 import time
 import numpy as np
+import win32com.client as wincl
 
 # Configure dates to search. Only counts business days non-holiday/break
 # API call url taken from XHR response
 start_date = '2020-07-06'
-end_date = '2020-08-20'
+end_date = '2020-08-10'
 date_url = f'https://calendly.com/api/booking/event_types/EEAPAX3DZQ2LU2G7/calendar/range?timezone=America%2FLos_Angeles&diagnostics=false&range_start={start_date}&range_end={end_date}&single_use_link_uuid='
 
 
@@ -21,3 +22,8 @@ for i in range(0, days + 1):
   if response['days'][i]['status'] == 'available':
     date = response['days'][i]['date']
     print(f"{date} has available slots")
+
+    speak = wincl.Dispatch("SAPI.SpVoice")
+    speak.Speak(f"{date} has available slots")
+
+
